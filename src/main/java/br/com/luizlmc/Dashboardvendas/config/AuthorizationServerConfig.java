8faa0d1724491a55ceac3f36731744a90e95d603
Luiz Carvalho <luiz.carvalho.lmc@gmail.com>
2022-01-12 22:43:47 -0300

    JWT com nome do usuário

--- src/main/java/br/com/luizlmc/Dashboardvendas/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/luizlmc/Dashboardvendas/config/AuthorizationServerConfig.java
@@ -12,11 +12,15 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@Profile("oauth-secutiry")
+import java.util.Arrays;
+
+@Profile("oauth-security")
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableAuthorizationServer
@@ -51,12 +55,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
         endpoints
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService)
-                .accessTokenConverter(accessTokenConverter())
                 .tokenStore(tokenStore())
-                .reuseRefreshTokens(false);
+                .tokenEnhancer(tokenEnhancerChain)
+                .reuseRefreshTokens(false)
+                .userDetailsService(userDetailsService)
+                .authenticationManager(authenticationManager);
     }
 
     @Bean
@@ -71,5 +78,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public TokenStore tokenStore() {
         return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
     }
 }
